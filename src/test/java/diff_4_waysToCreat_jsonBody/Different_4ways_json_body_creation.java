56515905d5b5dbbbe37b030f7ca098b8b0f99896
Raj <rvn.5jio@gmail.com>
2024-03-26 18:19:56 +0530

    adding remaing 3 ways of json creation

--- src/test/java/diff_4_waysToCreat_jsonBody/Different_4ways_json_body_creation.java
+++ src/test/java/diff_4_waysToCreat_jsonBody/Different_4ways_json_body_creation.java
@@ -5,14 +5,19 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.annotations.Test;
 
 import io.restassured.path.json.JsonPath;
 public class Different_4ways_json_body_creation {
-	String id=null;
-	@Test
+	int id;
+	//@Test(enabled = false)
 	void testPost_use_HashMap()
 	{
 		HashMap data = new HashMap();
@@ -54,10 +59,98 @@ public class Different_4ways_json_body_creation {
 		given()
 		
 		.when()
-			.delete("https://dummy.restapiexample.com/api/v1/delete/2"+id)
+			.delete("https://reqres.in/api/users/"+id)
 			
 		.then()
-		.statusCode(200);
+		.statusCode(204);
+		
+	}
+	
+	@Test(enabled = false)
+	void testPost_use_JsonLibrary()
+	{
+		JSONObject data = new JSONObject();
+		
+		data.put("name", "docker");
+		data.put("Location", "4444");
+		data.put("phone", "selenium/hub/chrome/safari:15.0");
+		data.put("email", "docker -e HUB_HOST");
+	
+		String cmds[] = {"pslink" , "prune", "pulldochub"};
+		
+		data.put("Docker", cmds);
+		
+		
+		given()
+				.contentType("application/json")
+				.body(data.toString())	
+			
+			
+			.when()
+				.post("https://reqres.in/api/users")  //https://dummy.restapiexample.com/api/v1/create
+			
+				
+			.then()
+				.statusCode(201)
+				.body("name", equalTo("docker"))
+				.body("Location", equalTo("4444"))
+				.body("phone", equalTo("selenium/hub/chrome/safari:15.0"))
+				
+				.body("Docker[0]", equalTo("pslink"))
+				.body("Docker[1]", equalTo("prune"))
+				.body("Docker[2]", equalTo("pulldochub"))
+				
+				
+				.header("Content-Type","application/json; charset=utf-8")
+				.log().all();
+		
+	}
+
+
+	@Test(enabled = false)
+	void test_Post_use_POJO()
+	{
+		
+		Pojo_postRequest pojo = new Pojo_postRequest();
+		pojo.setName("CICD");
+		pojo.setLocation("localhost:8080");
+		pojo.setPhone("git cred manager");
+		
+		String tech[] = {"vnc","brew","github" };
+		pojo.setTech(tech);
+		
+		id=given()
+				.contentType("application/json")
+				.body(pojo)
+		
+		.when()
+			.post("https://reqres.in/api/users/")
+			.jsonPath().getInt("id");		
+	}
+	
+	@Test
+	void test_Post_use_ExtJSON_file()
+	{
+		
+		File file =new File("/Users/rajeshvn/eclipse-RestAssured/Rest_Assured/Body.json");
+		try 
+		{
+			FileReader fr =new FileReader(file);
+			JSONTokener jt = new JSONTokener(fr);
+			JSONObject job = new JSONObject(jt);
+			
+			given()
+			
+			.when()
+				.contentType("application/json")
+				.body(job.toString());
+			
+			
+		} 
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}		
 		
 	}
 
---
+++ src/test/java/diff_4_waysToCreat_jsonBody/Pojo_postRequest.java
@@ -0,0 +1,38 @@
+package diff_4_waysToCreat_jsonBody;
+
+public class Pojo_postRequest 
+{
+	String name;
+	String Location;
+	String phone;
+	String tech[];
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String[] getTech() {
+		return tech;
+	}
+	public void setTech(String[] tech) {
+		this.tech = tech;
+	}
+	
+	
+	
+}
